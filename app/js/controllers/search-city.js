296ff51b789cadde034ba65c6a5495a7ba2f15ee
Karol Sikora <sicarrots@gmail.com>
2013-06-01 12:46:29 +0200

    scope fix

--- app/js/controllers/search-city.js
+++ app/js/controllers/search-city.js
@@ -12,17 +12,14 @@
 
     angular.module('rozkladyApp')
         .controller('SearchCityCtrl', function ($scope, $http, autocompleteCity) {
+            $scope.autocompleteResult = 'dupa';
             $scope.searchCity = function () {
                 $http({method: 'GET', url: 'http://infopasazer.intercity.pl/index_set.php?stacja=' + $scope.city})
                     .success(function (data) {
-                        $scope.$apply(function () {
-                            $scope.autocompleteResult = $scope.city;
-                        });
+                        $scope.autocompleteResult = $scope.city;
                     })
                     .error(function (data) {
-                        $scope.$apply(function () {
-                            $scope.autocompleteResult = 'error';
-                        });
+                        $scope.autocompleteResult = 'error';
                     });
             };
         });
