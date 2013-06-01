f8df4470c0748b6cdf03f435da62b7c931fd30fc
Karol Sikora <sicarrots@gmail.com>
2013-06-01 13:39:58 +0200

    autocomplete moved to service

--- app/js/controllers/search-city.js
+++ app/js/controllers/search-city.js
@@ -3,26 +3,26 @@
 
     angular.module('rozkladyApp')
         .factory('autocompleteCity', function ($http) {
-            return function (city) {
-//            http.get('http://infopasazer.intercity.pl/index_set.php?stacja=' + city, function (result) {
-//
-//            });
+            return {
+                async: function(city) {
+                    return $http.get(PROXY_URL + '/info_pasazer/index_set.php?stacja=' + city)
+                        .then(function (response) {
+                            var links = [];
+                            $($.parseHTML(response.data)).find('td a').each(function (_i, el) {
+                                links.push({name: $(el).text(), href: $(el).attr('href')});
+                            });
+                            return links;
+                        });
+                }
             };
         });
 
     angular.module('rozkladyApp')
         .controller('SearchCityCtrl', function ($scope, $http, autocompleteCity) {
             $scope.searchCity = function () {
-                $http.get(PROXY_URL + '/info_pasazer/index_set.php?stacja=' + $scope.city)
-                    .success(function (data) {
-                        $scope.autocompleteResult = [];
-                        $($.parseHTML(data)).find('td a').each(function (_i, el) {
-                            $scope.autocompleteResult.push({name: $(el).text(), href: $(el).attr('href')});
-                        });
-                    })
-                    .error(function (data) {
-                        $scope.autocompleteResult = 'error';
-                    });
+                autocompleteCity.async($scope.city).then(function (data) {
+                    $scope.autocompleteResult = data;
+                });
             };
         });
 })();
