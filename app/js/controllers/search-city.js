7ae2e647bbd2ab1bdbfdadf707fb86af276eefad
Karol Sikora <sicarrots@gmail.com>
2013-06-01 13:19:12 +0200

    autocomplete works

--- app/js/controllers/search-city.js
+++ app/js/controllers/search-city.js
@@ -14,9 +14,12 @@
         .controller('SearchCityCtrl', function ($scope, $http, autocompleteCity) {
             $scope.autocompleteResult = 'dupa';
             $scope.searchCity = function () {
-                $http({method: 'GET', url: 'http://infopasazer.intercity.pl/index_set.php?stacja=' + $scope.city})
+                $http({method: 'GET', url: 'http://127.0.0.1:8007/info_pasazer/index_set.php?stacja=' + $scope.city})
                     .success(function (data) {
-                        $scope.autocompleteResult = $scope.city;
+                        $scope.autocompleteResult = [];
+                        $($.parseHTML(data)).find('td a').each(function(_i, el){
+                            $scope.autocompleteResult.push({name: $(el).text(), href: $(el).attr('href')});
+                        });
                     })
                     .error(function (data) {
                         $scope.autocompleteResult = 'error';
